a4d3564475b427490fafaade8c1e90c5366a28bb
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-11-11 10:51:05 +1100

    fix python symlink

--- install/python.sh
+++ install/python.sh
@@ -8,4 +8,4 @@ add-apt-repository ppa:deadsnakes/ppa
 apt-get install -y python3.7 python3.7-dev python3.7-venv
 
 # create a symlink in /usr/local/bin which will take precedence on the path
-ln -s /usr/bin/python3.8 /usr/local/bin/python3
+ln -s /usr/bin/python3.7 /usr/local/bin/python3
