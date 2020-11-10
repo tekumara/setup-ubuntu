d473807e5e4398425ece07c259ad007ca0fd6c93
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-11-10 21:39:03 +1100

    fix: remove sudo from system.sh

--- install/system.sh
+++ install/system.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 
 set -x
 
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -f upgrade
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get -y -f upgrade
 
 # software-properties-common contains add-apt-repository and installs python
 # bsdmainutils contains column
 # net-tools contains ifconfig
 # iputils-ping contains ping
 # gnupg2 & ca-certificates needed for repos
-sudo apt-get install -y --no-install-recommends sysstat bsdmainutils jq net-tools iputils-ping curl \
-    gnupg2 ca-certificates software-properties-common 
+apt-get install -y --no-install-recommends sysstat bsdmainutils jq net-tools iputils-ping curl \
+    gnupg2 ca-certificates software-properties-common
