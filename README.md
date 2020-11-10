2c1bb8c92890e7f49778b3a58d79ed083cc8cf36
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-11-10 22:00:48 +1100

    add install.sh

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Includes:
 
 - CUDA install scripts for Ubuntu adapted from [nvidia/container-images/cuda](https://gitlab.com/nvidia/container-images/cuda/-/tree/master/dist/10.1/ubuntu18.04-x86_64)
+
+## Usage
+
+```
+curl -fsSL https://raw.githubusercontent.com/tekumra/setup-ubuntu/master/install.sh | sudo bash
+```
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -uoe pipefail
+
+mkdir -p "$INSTALL_DIR"
+
+URL="https://github.com/tekumara/setup-ubuntu/releases/latest/download/setup-ubuntu.tar.gz"
+
+if ! curl --progress-bar --fail -L "$URL" -o "/tmp/setup-ubuntu.tar.gz"; then
+    echo "Download failed.  Check that the release/filename are correct."
+    exit 1
+fi
+
+tar -xvf /tmp/setup-ubuntu.tar.gz -C /tmp
+
+set -x
+
+/tmp/setup-ubuntu/system.sh
+/tmp/setup-ubuntu/cuda.sh
