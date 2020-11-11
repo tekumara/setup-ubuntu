8026be93b7dc35f8bd5ee9a56a92c43408bab9e6
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-11-11 13:08:56 +1100

    install pip

--- install/python.sh
+++ install/python.sh
@@ -9,3 +9,8 @@ apt-get install -y python3.7 python3.7-dev python3.7-venv
 
 # create a symlink in /usr/local/bin which will take precedence on the path
 ln -s /usr/bin/python3.7 /usr/local/bin/python3
+ln -s /usr/bin/python3.7 /usr/local/bin/python
+
+# install pip directly, rather than installing the deb package which depends on the older python3 package
+# use sudo to make sure it is installed into dist-packages (/usr/local/lib/python3.7/dist-packages/)
+curl -s https://bootstrap.pypa.io/get-pip.py | sudo -H /usr/bin/python3.7
