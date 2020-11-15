33b72762e719d5680f34a838e83a0a2e74697bc8
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2020-11-15 21:46:42 +1100

    revert isolated python
    the extra complexity isn't worth it. most of the time virtualenvs will
    be in use, which ignore any system-installed packages (ie: dist-packages)

--- install/python.sh
+++ install/python.sh
@@ -12,33 +12,10 @@ apt-get update
 
 apt-get install -y --no-install-recommends python3.7 python3.7-dev python3.7-venv
 
-# the deadsnakes & ubuntu deb packages contain a patched version of site.py which adds /usr/lib/python3/dist-packages to PYTHONPATH
-# see https://github.com/deadsnakes/python3.7/blob/4dc651768517acccad5f5081fff2de3e4d5900cd/debian/patches/distutils-install-layout.diff#L243
-# this means any python3-* package installed via apt will end up in /usr/lib/python3/dist-packages and so on our PYTHONPATH
-# we want an isolated version without this, so create a parallel version in /usr/local with the unpatched libs
-
-# install the unpatched source libs into /usr/local
-PYTHON_VERSION=3.7.9
-wget -q -O python.tar.xz "https://www.python.org/ftp/python/${PYTHON_VERSION%%[a-z]*}/Python-$PYTHON_VERSION.tar.xz"
-mkdir -p /usr/src/python
-tar -xJC /usr/src/python --strip-components=1 -f python.tar.xz
-cp -r /usr/src/python/Lib/* /usr/local/lib/python3.7/
-rm python.tar.xz
-rm -rf /usr/src/python
-# remove empty dist-packages dir
-rm -rf /usr/local/lib/python3.7/dist-packages
-
-# copy built artifacts to /usr/local
-mkdir /usr/local/lib/python3.7/lib-dynload/
-cp /usr/lib/python3.7/lib-dynload/* /usr/local/lib/python3.7/lib-dynload/
-cp /usr/lib/python3.7/_sysconfigdata_m_linux_x86_64-linux-gnu.py /usr/local/lib/python3.7/
-# copy the binary, which sets the prefix as /usr/local
-cp /usr/bin/python3.7 /usr/local/bin/python3.7
-
-# convenience symlinks
-[[ ! -f /usr/local/bin/python ]] && (cd /usr/local/bin && ln -s python3.7 python)
-[[ ! -f /usr/local/bin/python3 ]] && (cd /usr/local/bin && ln -s python3.7 python3)
+# create symlinks in /usr/local/bin which will take precedence on the path
+[[ ! -f /usr/local/bin/python3 ]] && ln -s /usr/bin/python3.7 /usr/local/bin/python3
+[[ ! -f /usr/local/bin/python ]] && ln -s /usr/bin/python3.7 /usr/local/bin/python
 
 # install pip directly, rather than installing the deb package which depends on the older python3 package
-# use sudo to make sure it is installed as a system rather than user package (ie: /usr/local/lib/python3.7/site-packages/)
+# use sudo to make sure it is installed as a system rather than user package (ie: /usr/local/lib/python3.7/dist-packages/)
 curl -s https://bootstrap.pypa.io/get-pip.py | sudo -H /usr/local/bin/python3
