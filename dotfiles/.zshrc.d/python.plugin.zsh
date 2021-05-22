799e1e7102df64aea40b41a61eb487fe0abf7e9c
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-05-22 16:51:37 +1000

    add mkvenv alias

--- dotfiles/.zshrc.d/python.plugin.zsh
+++ dotfiles/.zshrc.d/python.plugin.zsh
@@ -1,8 +1,10 @@
 # prevent pip from installing globally
 export PIP_REQUIRE_VIRTUALENV=true
 
-# activate virtualenv in ./venv/
-alias venv='. venv/bin/activate'
+# activate virtualenv in .venv/ or venv/
+alias venv='{[[ -d .venv ]] && . .venv/bin/activate} || {[[ -d venv ]] && . venv/bin/activate} || echo "Missing .venv/"'
+# create venv - if a venv is active pip won't be installed so check first
+alias mkvenv='[[ -z "${VIRTUAL_ENV}" ]] && python3 -m venv --clear .venv || echo Deactivate active virtualenv first'
 
 # load virtualenvwrapper
 VIRTUALENVWRAPPER_PYTHON=$HOME/.local/pipx/venvs/virtualenvwrapper/bin/python
