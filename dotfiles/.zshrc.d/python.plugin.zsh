f6c139f7623f02b81d9fd1ad3d85e11dca20e718
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-05-24 16:03:01 +1000

    mkvenv: use virtualenv

--- dotfiles/.zshrc.d/python.plugin.zsh
+++ dotfiles/.zshrc.d/python.plugin.zsh
@@ -3,8 +3,8 @@ export PIP_REQUIRE_VIRTUALENV=true
 
 # activate virtualenv in .venv/ or venv/
 alias venv='{[[ -d .venv ]] && . .venv/bin/activate} || {[[ -d venv ]] && . venv/bin/activate} || echo "Missing .venv/"'
-# create venv - if a venv is active pip won't be installed so check first
-alias mkvenv='[[ -z "${VIRTUAL_ENV}" ]] && python3 -m venv --clear .venv || echo Deactivate active virtualenv first'
+# create venv
+alias mkvenv='virtualenv .venv'
 
 # load virtualenvwrapper
 VIRTUALENVWRAPPER_PYTHON=$HOME/.local/pipx/venvs/virtualenvwrapper/bin/python
