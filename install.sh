a11bfda14036ab95c735c8c634932ce9cf1164d6
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-10-15 20:56:31 +1100

    install.sh tidy casing

--- install.sh
+++ install.sh
@@ -45,9 +45,9 @@ if [[ "$USER" == "root" && -z "${INSTALL_USER-}" ]]; then
     die "When run as root must specify an install user via -u"
 fi
 
-TARBALL_URL="https://github.com/tekumara/setup-ubuntu/tarball/$GIT_SHA"
-echo "Downloading $TARBALL_URL"
-if ! curl --progress-bar --fail -L "$TARBALL_URL" -o "/tmp/setup-ubuntu.tar.gz"; then
+tarball_url="https://github.com/tekumara/setup-ubuntu/tarball/$GIT_SHA"
+echo "Downloading $tarball_url"
+if ! curl --progress-bar --fail -L "$tarball_url" -o "/tmp/setup-ubuntu.tar.gz"; then
     die "Download failed.  Check that the URL is correct."
 fi
 
