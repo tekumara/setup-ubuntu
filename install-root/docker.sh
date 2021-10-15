3688f5892901f4099efa69e6d5ef936399be41ae
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-10-15 20:54:58 +1100

    feat: add docker-compose v2.0.1

--- install-root/docker.sh
+++ install-root/docker.sh
@@ -9,3 +9,9 @@ echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $VERSION_CODENAM
 apt-get update
 
 apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io
+
+# docker compose
+v=v2.0.1
+curl -fsSLo /usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$v/docker-compose-linux-x86_64"
+echo "b2aabbc04768efb0dc73c14d105781dd0aceb7a801d3a27d5113ad8e222f0395  /usr/local/bin/docker-compose"  | sha256sum --check
+chmod a+x /usr/local/bin/docker-compose
