5b4cd2b84f0f7609653d7fd99c107221fbf5ce80
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-10-29 10:58:33 +1100

    feat: add docker, kubes, python aliases

--- dotfiles/.zshrc.d/kubes.plugin.zsh
+++ dotfiles/.zshrc.d/kubes.plugin.zsh
@@ -4,16 +4,22 @@
 # https://github.com/ahmetb/kubectl-aliases
 alias k='kubectl'
 alias kc='kubectx'
-alias kdp='kubectl delete pod'
+alias kd='kubectl describe'
+alias kdd='kubectl describe deployment'
+alias kdj='kubectl describe job'
+alias kdn='kubectl describe node'
+alias kdp='kubectl describe pod'
 alias kg='kubectl get'
-alias kgp='kubectl get pod'
-alias kgpa='kubectl get pod --all-namespaces'
 alias kgd='kubectl get deployment'
 alias kgda='kubectl get deployment --all-namespaces'
+alias kgj='kubectl get job'
+alias kgn='kubectl get node'
+alias kgp='kubectl get pod'
+alias kgpa='kubectl get pod --all-namespaces'
 alias kgs='kubectl get service'
 alias kgsa='kubectl get service --all-namespaces'
 alias kge='kubectl get events --sort-by='{.lastTimestamp}''
-alias kgj='kubectl get job'
+alias kcomp='type __kubectl_compgen  > /dev/null || source <(kubectl completion zsh)'
 
 keb() {
   kubectl exec -i -t "$@" -- /bin/bash
@@ -30,13 +36,14 @@ krs() {
   kubectl run "$1" -it --image="$2" --command -- /bin/sh
 }
 
+# unset is needed becuase we are appending values below and
+# it may already have been set by the  parent process (eg: vscode)
 unset KUBECONFIG
+
 files=($HOME/.kube/*.yaml(N) $HOME/.k3d/kubeconfig*.yaml(N))
 for file in $files; do
   KUBECONFIG+="${KUBECONFIG+:}${file}"
 done
-
-export KUBECONFIG
 
 # kube context per shell https://github.com/ahmetb/kubectx/issues/12#issuecomment-557852519
 file="$(mktemp -t "kubectx.XXXXXX")"
--- dotfiles/.zshrc.d/python.plugin.zsh
+++ dotfiles/.zshrc.d/python.plugin.zsh
@@ -6,6 +6,20 @@ alias venv='{[[ -d .venv ]] && . .venv/bin/activate} || {[[ -d venv ]] && . venv
 # create venv and activate
 alias mkvenv='virtualenv --clear .venv && . .venv/bin/activate'
 
+# vscode debugger
+debugpy() {
+    local port=62888
+    if [[ -z $VIRTUAL_ENV ]]; then
+        echo Could not find an activated virtualenv
+    else
+        [[ -d $VIRTUAL_ENV/lib/python*/site-packages/debugpy(#qN) ]] || (echo "Installing debugpy" && pip install debugpy)
+        echo "Attach vscode debugger to port $port" >&2
+        python -m debugpy --listen "$port" --wait-for-client "$@"
+    fi
+}
+
+alias pyright=node_modules/.bin/pyright
+
 # load virtualenvwrapper
 VIRTUALENVWRAPPER_PYTHON=$HOME/.local/pipx/venvs/virtualenvwrapper/bin/python
 source "$HOME/.local/bin/virtualenvwrapper.sh"
