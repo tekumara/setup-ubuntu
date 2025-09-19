5f5188d62e1218bd8292e55b504f246793c27c1e
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-09-19 12:46:56 +1000

    fix: Update docker.sh
    actually install

--- install-root/docker.sh
+++ install-root/docker.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2174,SC1091
 
 set -euo pipefail
 
 # add docker stable repo
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-source /etc/os-release
-echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $VERSION_CODENAME stable" > "/etc/apt/sources.list.d/docker-stable-$VERSION_CODENAME.list"
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list
+
 apt-get update
 
-apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io
+apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# docker compose
-v=v2.4.0
-curl -fsSLo /usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$v/docker-compose-linux-x86_64"
-echo "83527297959403d20a4fd4f00d3db6d9bbbc0cc5704787ec4396cd706a6c8bb4  /usr/local/bin/docker-compose"  | sha256sum --check
-chmod a+x /usr/local/bin/docker-compose
-# integrate into the docker cli as `docker compose`
-#ln -s /usr/local/bin/docker-compose /usr/libexec/docker/cli-plugins/docker-compose
+# enable usages of standalone "docker-compose" by linking to the plugin
+[[ -f /usr/local/bin/docker-compose ]] || ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
